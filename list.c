dce27b465a9b440d7730c0eb1f351eee6a3e5b1b
merida <maxxiids@outlook.es>
2023-03-04 04:52:48 -0300

    update list.c

--- list.c
+++ list.c
@@ -14,5 +14,15 @@ int main(){
         memcpy(list,"list",5)
         printf("list : %s\n",list);
 
+	char * list = malloc(strlen("list")+1);
+	memset(list,0,strlen("list")+1);
+	memcpy(list,"list",5);
+	printf("list : %s\n",list);
+
+        char * list = malloc(strlen("list")+1);
+	memset(list,0,strlen("list")+1);
+        memcpy(list,"list",5)
+        printf("list : %s\n",list);
+
 	return 0;
 }
